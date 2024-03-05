cc74643b17a43bfe4340e86eea93243f417bb76c
Iker García Calviño <iker.gcalvino@udc.es>
2024-03-05 13:41:58 +0100

    Ajustes merge

--- app/src/main/java/com/muei/soundshare/ui/signup/SignupFragment.kt
+++ app/src/main/java/com/muei/soundshare/ui/signup/SignupFragment.kt
@@ -5,27 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.muei.soundshare.databinding.FragmentHomeBinding
 import com.muei.soundshare.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
 
     private var _binding: FragmentSignupBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
 
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/muei/soundshare/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/muei/soundshare/ui/signup/SignupViewModel.kt
@@ -1,13 +1,5 @@
 package com.muei.soundshare.ui.signup
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SignupViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is signup Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class SignupViewModel : ViewModel()
