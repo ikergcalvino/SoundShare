306f32f088b0d887fdd26a5fe4c2f6f8c43fd4b5
Mar Alonso <mar.alonso@udc.es>
2024-03-05 19:53:47 +0100

    Pop up de shazam

--- app/src/main/java/com/muei/soundshare/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/muei/soundshare/ui/notifications/NotificationsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,8 +16,6 @@ class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
-    private val binding get() = _binding!!
-
     private val notifications = listOf(
         Notification("Notificacion 1"),
         Notification("Notificacion 2"),
@@ -27,19 +26,16 @@ class NotificationsFragment : Fragment() {
     )
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View {
-        val notificationsViewModel = ViewModelProvider(this)[NotificationsViewModel::class.java]
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_notifications, container, false)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerNotifications)
         val adapter = NotificationsAdapter(notifications)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        return binding.root
+        return view
     }
 
     override fun onDestroyView() {
