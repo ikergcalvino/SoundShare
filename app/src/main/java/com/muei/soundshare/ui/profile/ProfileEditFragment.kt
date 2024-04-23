017c51ed4dfab37535189d7cd0650cf8905510e4
david_esteban_martinez <david.eseban.martinez@udc.es>
2024-04-23 18:38:07 +0200

    Cambiada la interfaz de edición de usuarios para que guardar los ajustes se haga con un botón flotante

--- app/src/main/java/com/muei/soundshare/ui/profile/ProfileEditFragment.kt
+++ app/src/main/java/com/muei/soundshare/ui/profile/ProfileEditFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.muei.soundshare.R
 import com.muei.soundshare.databinding.FragmentProfileEditBinding
 
 class ProfileEditFragment : Fragment() {
@@ -23,24 +22,12 @@ class ProfileEditFragment : Fragment() {
 
         _binding = FragmentProfileEditBinding.inflate(inflater, container, false)
 
-        binding.topNav.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.navigation_check -> {
-                    Log.d("SoundShare", "Confirm button clicked")
+        binding.buttonSave.setOnClickListener {
+            Log.d("SoundShare", "Save button clicked")
 
-                    // ProfileService para la actualización asíncrona
+            // ProfileService para la actualización asíncrona
 
-                    findNavController().navigate(R.id.navigation_profile)
-                    true
-                }
-
-                else -> false
-            }
-        }
-
-        binding.topNav.setNavigationOnClickListener {
-            Log.d("SoundShare", "Cancel button clicked")
-            findNavController().navigate(R.id.navigation_profile)
+            findNavController().navigateUp()
         }
 
         return binding.root
