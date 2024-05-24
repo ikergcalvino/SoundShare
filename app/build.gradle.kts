b1284b4daa3abd23b919860c7e86a4f7b7b5c307
Mar Alonso <mar.alonso@udc.es>
2024-05-24 21:31:42 +0200

    Añadida funcionalidad de reconocer canción

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation(files("libs/acrcloud-universal-sdk-1.3.26.jar"))
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -49,9 +51,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("org.chromium.net:cronet-embedded:119.6045.31")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-analytics")
--- app/src/main/java/com/muei/soundshare/MainActivity.kt
+++ app/src/main/java/com/muei/soundshare/MainActivity.kt
@@ -1,28 +1,59 @@
 package com.muei.soundshare
 
-
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Base64
 import android.util.Log
 import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.acrcloud.rec.ACRCloudClient
+import com.acrcloud.rec.ACRCloudConfig
+import com.acrcloud.rec.ACRCloudResult
+import com.acrcloud.rec.IACRCloudListener
+import com.acrcloud.rec.utils.ACRCloudLogger
+import com.android.volley.BuildConfig
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.muei.soundshare.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IACRCloudListener {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var topNav: MaterialToolbar
     private lateinit var bottomNav: BottomNavigationView
     private lateinit var navController: NavController
+    private var isRecording = false
+    private var progressDialog: AlertDialog? = null
+    private var audioRecord: AudioRecord? = null
+    private val sampleRate = 44100
+    private val channelConfig = AudioFormat.CHANNEL_IN_MONO
+    private val audioFormat = AudioFormat.ENCODING_PCM_16BIT
+    private var mClient: ACRCloudClient? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -132,19 +163,99 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.buttonShazam.setOnClickListener {
-            Log.d("SoundShare", "Shazam button clicked")
-            val dialogView = layoutInflater.inflate(R.layout.layout_song, null)
 
-            MaterialAlertDialogBuilder(this).setTitle("Title").setView(dialogView)
-                .setMessage("Support Text")
-                .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
-                    dialog.dismiss()
-                }.setNeutralButton(getString(R.string.post)) { dialog, _ ->
-                    dialog.dismiss()
-                }.setPositiveButton(getString(R.string.ok)) { dialog, _ ->
-                    dialog.dismiss()
-                }.show()
+        binding.buttonShazam.setOnClickListener {
+            initAcrcloud()
+            startRecognition()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        progressDialog?.dismiss()
+        progressDialog = null
+    }
+
+
+    private fun initAcrcloud() {
+        val config = ACRCloudConfig()
+
+        config.acrcloudListener = this
+        config.context = this
+
+        config.host = "identify-eu-west-1.acrcloud.com"
+        config.accessKey = "a677b38c71a0a38b3092106b31f758ec"
+        config.accessSecret = "ENNzTVtGpr0y1tvnTgWjYhkiyQQyu98oZdI9ycAN"
+
+        config.recorderConfig.rate = 8000
+        config.recorderConfig.channels = 1
+
+        mClient = ACRCloudClient()
+        if (BuildConfig.DEBUG) {
+            ACRCloudLogger.setLog(true)
+        }
+        mClient!!.initWithConfig(config)
+    }
+
+    private fun startRecognition() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Solicitar el permiso para grabar audio
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                200
+            )
+        }
+        mClient?.let {
+            if (it.startRecognize()) {
+                // Mostrar diálogo de progreso
+                progressDialog = AlertDialog.Builder(this).apply {
+                    setTitle("Analizando audio...")
+                    setView(ProgressBar(this@MainActivity))
+                    setCancelable(false)
+                }.create()
+                progressDialog?.show()
+            } else {
+                Log.e("SoundShare", "Init error")
+            }
+        } ?: run {
+            Log.e("SoundShare", "Client not ready")
+        }
+    }
+
+    override fun onResult(acrResult: ACRCloudResult?) {
+        acrResult?.let {
+            Log.d("SoundShare", "ACRCloud result received: ${it.result}")
+            handleResult(it.result)
+        }
+    }
+
+    override fun onVolumeChanged(vol: Double) {
+        Log.d("SoundShare", "Volume changed $vol")
+    }
+
+    private fun handleResult(acrResult: String) {
+        // Parse the JSON response
+        val jsonObject = JSONObject(acrResult)
+        val metadata = jsonObject.getJSONObject("metadata")
+        val humming = metadata.getJSONArray("humming")
+        val firstResult = humming.getJSONObject(0)
+        val songTitle = firstResult.getString("title")
+
+        // Close the progress dialog
+        progressDialog?.dismiss()
+
+        // Show the song title in a new dialog
+        AlertDialog.Builder(this).apply {
+            setTitle("Song Identified")
+            setMessage(songTitle)
+            setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+        }.show()
     }
 }
