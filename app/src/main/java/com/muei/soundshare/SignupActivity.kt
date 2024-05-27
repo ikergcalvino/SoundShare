5eb5b0ca5db74f567b8c9fec0848f319ecd3aa0f
Iker García Calviño <iker.gcalvino@udc.es>
2024-05-28 00:31:43 +0200

    Primera aproximación registro

--- app/src/main/java/com/muei/soundshare/SignupActivity.kt
+++ app/src/main/java/com/muei/soundshare/SignupActivity.kt
@@ -1,51 +1,38 @@
 package com.muei.soundshare
 
-/*import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.gson.GsonFactory
-import com.google.api.services.people.v1.PeopleService
-import com.google.api.services.people.v1.model.Person
-import com.google.api.services.people.v1.PeopleServiceRequestInitializer*/
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.FirebaseFirestore
 import com.muei.soundshare.databinding.ActivitySignupBinding
+import com.muei.soundshare.entities.User
 import com.muei.soundshare.util.Constants
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
-    private lateinit var loadingOverlay: View
 
-    private val firebaseAuth: FirebaseAuth by inject()
+    private val firebase: FirebaseAuth by inject()
+    private val firestore: FirebaseFirestore by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val db = Firebase.firestore
-
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        loadingOverlay = findViewById(R.id.loadingOverlay)
+        val loadingOverlay: View = findViewById(R.id.loadingOverlay)
 
         binding.dateOfBirthLayout.setStartIconOnClickListener {
             val datePickerDialog = MaterialDatePicker.Builder.datePicker()
@@ -54,7 +41,11 @@ class SignupActivity : AppCompatActivity() {
 
             // Handle selected date
             datePickerDialog.addOnPositiveButtonClickListener { selectedDate ->
-                binding.textDateOfBirth.setText(datePickerDialog.headerText)
+                binding.textDateOfBirth.setText(
+                    SimpleDateFormat(Constants.DATE_PATTERN, Locale.getDefault()).format(
+                        Date(selectedDate)
+                    )
+                )
             }
 
             datePickerDialog.show(supportFragmentManager, "DATE_PICKER")
@@ -63,259 +54,134 @@ class SignupActivity : AppCompatActivity() {
         binding.buttonSignUp.setOnClickListener {
             Log.d("SoundShare", "Sign up button clicked")
 
-            if (validateFields()) {
+            val username = binding.textUsername.text.toString()
+            val email = binding.textEmail.text.toString()
+            val password = binding.textPassword.text.toString()
+            val repeatPassword = binding.textRepeatPassword.text.toString()
+            val dateOfBirth = binding.textDateOfBirth.text.toString()
+            val phoneNumber = binding.textPhoneNumber.text.toString()
 
-                val email = binding.textEmail.text.toString()
-                val password = binding.textPassword.text.toString()
-                val username = binding.textUsername.text.toString()
-                val dateOfBirth = binding.textDateOfBirth.text.toString()
-                val phoneNumber = binding.textPhoneNumber.text.toString()
+            if (validateFields(username, email, password, repeatPassword, dateOfBirth)) {
+                loadingOverlay.visibility = View.VISIBLE
 
-                val user = hashMapOf(
-                    "email" to email,
-                    "username" to username,
-                    "dateOfBirth" to dateOfBirth,
-                    "phoneNumber" to phoneNumber,
-                    "profileImage" to ""
-                )
+                firebase.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val userId = firebase.currentUser?.uid ?: ""
+                            val dateOfBirthDate =
+                                SimpleDateFormat(Constants.DATE_PATTERN, Locale.getDefault()).parse(
+                                    dateOfBirth
+                                )
 
-                val userDocRef = db.collection("users").document(email)
-                userDocRef.get().addOnCompleteListener { docTask ->
-                    if (docTask.isSuccessful) {
-                        val document = docTask.result
-                        if (document.exists()) {
-                            Toast.makeText(
-                                baseContext,
-                                "Username already exists.",
-                                Toast.LENGTH_LONG,
-                            ).show()
-                        } else {
+                            val user = User(
+                                username = username,
+                                email = email,
+                                password = password,
+                                dateOfBirth = dateOfBirthDate!!,
+                                phoneNumber = phoneNumber
+                            )
 
-                            firebaseAuth.createUserWithEmailAndPassword(email, password)
-                                .addOnCompleteListener(this) { task ->
-                                    if (task.isSuccessful) {
-                                        CoroutineScope(Dispatchers.Main).launch {
-                                            delay(2000)
-                                            showLoading(true)
-                                            db.collection("users")
-                                                .document(email).set(user)
-                                                .addOnSuccessListener { documentReference ->
-                                                    Log.d(
-                                                        "SoundShare",
-                                                        "DocumentSnapshot added with ID: $email"
-                                                    )
-                                                }
-                                                .addOnFailureListener { e ->
-                                                    Log.w("SoundShare", "Error adding document", e)
-                                                }
-                                            val mainIntent =
-                                                Intent(
-                                                    this@SignupActivity,
-                                                    MainActivity::class.java
-                                                )
-                                            startActivity(mainIntent)
-                                            finish()
-                                        }
-                                    } else {
-                                        Toast.makeText(
-                                            baseContext,
-                                            "Sign up failed.",
-                                            Toast.LENGTH_LONG,
-                                        ).show()
-                                    }
+                            firestore.collection("users").document(userId).set(user)
+                                .addOnSuccessListener {
+                                    startActivity(
+                                        Intent(
+                                            this@SignupActivity, LoginActivity::class.java
+                                        )
+                                    )
+                                    finish()
                                 }
+                        } else {
+                            loadingOverlay.visibility = View.GONE
+                            Toast.makeText(
+                                this,
+                                "Sign Up Failed: ${task.exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
                         }
                     }
-                }
             }
-        }
-
-        binding.buttonSignUpWithGoogle.setOnClickListener {
-            Log.d("SoundShare", "Sign up with Google button clicked")
-
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("560113047004-kssj6q8mnkmoqn6lhbvqlnvfhhv8sq21.apps.googleusercontent.com")
-                .requestEmail()
-                .build()
-
-            val googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-            // Intent para iniciar el flujo de inicio de sesión de Google
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, Constants.RC_SIGN_IN)
-
-            val mainIntent = Intent(this@SignupActivity, MainActivity::class.java)
-            startActivity(mainIntent)
-            finish()
         }
 
         binding.buttonLogInHere.setOnClickListener {
             Log.d("SoundShare", "Log in here button clicked")
+
             val loginIntent = Intent(this@SignupActivity, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun validateFields(
+        username: String,
+        email: String,
+        password: String,
+        repeatPassword: String,
+        dateOfBirth: String
+    ): Boolean {
 
-        if (requestCode == Constants.RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In fue exitoso, autenticar con Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In falló
-                Log.w("SoundShare", "Google sign in failed", e)
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Inicio de sesión exitoso, actualizar UI con la información del usuario firmado
-                    Log.d("SoundShare", "signInWithCredential:success")
-                    val user = firebaseAuth.currentUser
-
-                    /*val netHttpTransport = NetHttpTransport()
-                    val gsonFactory = GsonFactory()
-
-                    val googleAccount = GoogleSignIn.getLastSignedInAccount(this)
-                    val accessToken = googleAccount?.idToken
-
-                    val credential1 = GoogleCredential().setAccessToken(accessToken)
-
-                    val peopleService = PeopleService.Builder(netHttpTransport, gsonFactory, credential1)
-                        .setApplicationName("SoundShare")
-                        .setGoogleClientRequestInitializer(PeopleServiceRequestInitializer(accessToken))
-                        .build()
-
-                    val person: Person = peopleService.people().get("people/me")
-                        .setPersonFields("birthdays")
-                        .execute()
-
-                    val birthdays = person.birthdays
-                    for (birthday in birthdays) {
-                        if (birthday.date != null) {
-                            val date = birthday.date
-                            val day = date.day
-                            val month = date.month
-                            val year = date.year
-
-                            println("User's birthday is $day/$month/$year")
-                            Log.d("SoundShare", "User's birthday is $day/$month/$year")
-                        }
-                    }*/
-                } else {
-                    // Si el inicio de sesión falla, mostrar un mensaje al usuario
-                    Log.w("SoundShare", "signInWithCredential:failure", task.exception)
-                    // ...
-                }
-            }
-    }
-
-    private fun validateFields(): Boolean {
-        val email = binding.textEmail.text.toString()
-        val username = binding.textUsername.text.toString()
-        val password = binding.textPassword.text.toString()
-        val confirmPassword = binding.textRepeatPassword.text.toString()
-        val dateOfBirth = binding.textDateOfBirth.text.toString()
-        val red = 239
-        val green = 119
-        val blue = 113
-
-        if (email.isEmpty()) {
-            Toast.makeText(this, getString(R.string.email_required), Toast.LENGTH_LONG).show()
-            binding.textEmail.setBackgroundColor(Color.rgb(red, green, blue))
-            return false
-        }
+        binding.emailLayout.error = null
+        binding.textEmail.error = null
+        binding.usernameLayout.error = null
+        binding.textUsername.error = null
+        binding.passwordLayout.error = null
+        binding.textPassword.error = null
+        binding.repeatPasswordLayout.error = null
+        binding.textRepeatPassword.error = null
+        binding.dateOfBirthLayout.error = null
+        binding.textDateOfBirth.error = null
 
         if (username.isEmpty()) {
-            Toast.makeText(this, getString(R.string.username_required), Toast.LENGTH_SHORT).show()
-            binding.textUsername.setBackgroundColor(Color.rgb(red, green, blue))
+            binding.usernameLayout.error = getString(R.string.username_required)
             return false
         }
 
         if (username.length < 6) {
-            Toast.makeText(this, getString(R.string.username_length), Toast.LENGTH_SHORT).show()
-            binding.textUsername.setBackgroundColor(Color.rgb(red, green, blue))
+            binding.textUsername.error = getString(R.string.username_length)
+            return false
+        }
+
+        if (email.isEmpty()) {
+            binding.emailLayout.error = getString(R.string.email_required)
             return false
         }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            Toast.makeText(this, getString(R.string.invalid_email), Toast.LENGTH_SHORT).show()
-            binding.textEmail.setBackgroundColor(Color.rgb(red, green, blue))
-            return false
-        }
-
-        if (password.length < 8) {
-            Toast.makeText(this, getString(R.string.password_length), Toast.LENGTH_SHORT).show()
-            binding.textPassword.setBackgroundColor(Color.rgb(red, green, blue))
+            binding.textEmail.error = getString(R.string.invalid_email)
             return false
         }
 
         if (password.isEmpty()) {
-            Toast.makeText(this, getString(R.string.password_required), Toast.LENGTH_SHORT).show()
-            binding.textPassword.setBackgroundColor(Color.rgb(red, green, blue))
+            binding.passwordLayout.error = getString(R.string.password_required)
             return false
         }
 
-        if (confirmPassword.isEmpty()) {
-            Toast.makeText(this, getString(R.string.passwords_dont_match), Toast.LENGTH_SHORT)
-                .show()
-            binding.textRepeatPassword.setBackgroundColor(Color.rgb(red, green, blue))
+        if (password.length < 8) {
+            binding.textPassword.error = getString(R.string.password_length)
+            return false
+        }
+
+        if (repeatPassword.isEmpty()) {
+            binding.repeatPasswordLayout.error = getString(R.string.passwords_dont_match)
+            return false
+        }
+
+        if (password != repeatPassword) {
+            binding.textRepeatPassword.error = getString(R.string.passwords_dont_match)
             return false
         }
 
         if (dateOfBirth.isEmpty()) {
-            Toast.makeText(this, getString(R.string.date_of_birth_required), Toast.LENGTH_SHORT)
-                .show()
-            binding.textDateOfBirth.setBackgroundColor(Color.rgb(red, green, blue))
+            binding.dateOfBirthLayout.error = getString(R.string.date_of_birth_required)
             return false
         }
 
-        // TODO: Poner esto más exacto
         val dateOfBirthArray = dateOfBirth.split("/")
-        val year = dateOfBirthArray[2].toInt()
-        val currentYear = 2024
-        if (currentYear - year < 18) {
-            Toast.makeText(this, getString(R.string.age_restriction), Toast.LENGTH_SHORT).show()
-            binding.textDateOfBirth.setBackgroundColor(Color.rgb(red, green, blue))
-            return false
-        }
-
-        if (password != confirmPassword) {
-            Toast.makeText(this, getString(R.string.passwords_dont_match), Toast.LENGTH_SHORT)
-                .show()
-            binding.textRepeatPassword.setBackgroundColor(Color.rgb(red, green, blue))
+        if (Calendar.getInstance().get(Calendar.YEAR) - dateOfBirthArray[2].toInt() < 18) {
+            binding.textDateOfBirth.error = getString(R.string.age_restriction)
             return false
         }
 
         return true
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = firebaseAuth.currentUser
-        if (currentUser != null) {
-            val mainIntent = Intent(this@SignupActivity, MainActivity::class.java)
-            startActivity(mainIntent)
-            finish()
-        }
-    }
-
-    private fun showLoading(show: Boolean) {
-        if (show) {
-            loadingOverlay.visibility = View.VISIBLE
-        } else {
-            loadingOverlay.visibility = View.GONE
-        }
     }
 
 }
